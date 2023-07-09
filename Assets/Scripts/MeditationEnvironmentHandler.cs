f9f2bd4955f875790850300fdc68c6ee227a3ce1
Larissa <larissamariagross@gmail.com>
2023-07-09 16:00:42 +0200

    added meditation voice audio switch through voice command (tied to meditation scene)

--- Assets/Scripts/MeditationEnvironmentHandler.cs
+++ Assets/Scripts/MeditationEnvironmentHandler.cs
@@ -16,13 +16,30 @@ public class MeditationEnvironmentHandler : MonoBehaviour
     private GameObject menu;
 
     private GameObject activeScene;
+
+    [SerializeField]
+    private AudioClip seaAudioVoice;
+
+    [SerializeField]
+    private AudioClip gardenAudioVoice;
+
+    [SerializeField]
+    private AudioClip menuAudioVoice;
+
+    private GameObject meditationAudio;
+    private AudioSource meditationAudioSource;
+
     private void Start()
     {
         activeScene = menu;
+        meditationAudio = GameObject.Find("Meditation Voice Audio");
+        meditationAudioSource = meditationAudio.GetComponent<AudioSource>();
     }
+
     /// <summary>
     /// Checks wether the string that is captured through the microphone corresponds to sea, garden or menu and their
-    /// synonyms. Destroys the active scene as soon as a different scene is chosen.
+    /// synonyms. Destroys the active scene as soon as a different scene is chosen and sets the scene to the game object
+    /// that was set in the Inspector, as well as the audio clipt that was set in the Inspector.
     /// </summary>
     /// <param name="values"></param>
     public void UpdateMeditationScene(string[] values)
@@ -37,17 +54,20 @@ public class MeditationEnvironmentHandler : MonoBehaviour
             {
                 GameObject.Destroy(activeScene);
                 activeScene = Instantiate(seaMeditation, new Vector3(0, 0, 0), Quaternion.identity);
+                meditationAudioSource.clip = seaAudioVoice;
             }
             else if (sceneString == "garden" || sceneString == "nature")
             {
                 GameObject.Destroy(activeScene);
                 activeScene = Instantiate(gardenMeditation, new Vector3(0, 0, 0), Quaternion.identity);
-
+                meditationAudioSource.clip = gardenAudioVoice;
+                //new Vector3(-1858, -18, -288)
             }
             else if (sceneString == "menu" || sceneString == "back" || sceneString == "home")
             {
                 GameObject.Destroy(activeScene);
                 activeScene = Instantiate(menu, new Vector3(0, 0, 0), Quaternion.identity);
+                meditationAudioSource.clip = menuAudioVoice;
             }
         }
     }
